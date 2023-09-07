7a4e315fa5e94e7c8c1f69f88f7f38827487d5e1
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2023-09-07 11:40:10 +0300

    ekat projektilisäykset

---
+++ testispring/src/main/java/hh/sof03/testispring/TestispringApplication.java
@@ -0,0 +1,13 @@
+package hh.sof03.testispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestispringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestispringApplication.class, args);
+	}
+
+}
---
+++ testispring/src/test/java/hh/sof03/testispring/TestispringApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.sof03.testispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestispringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
